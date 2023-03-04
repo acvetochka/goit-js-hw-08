ba2343e586d122ea10430e8c7673e8cc76847f32
Alona Kuznietsova <acvetochka@gmail.com>
2023-03-04 21:55:49 +0100

    add task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </div>`;
+    })
+    .join('');
+}
+
+//add items of img to HTML
+const gallery = document.querySelector('.gallery');
+const items = createGalleryItem(galleryItems);
+gallery.insertAdjacentHTML('beforeend', items);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  // captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
